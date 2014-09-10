36ec8a55a578ca4f96c707b69048f2510815d56a
Vinicius Munhoz <viniciusfmunhoz@gmail.com>
2014-09-10 00:55:32 -0300

    Estrutura da pagina inicial

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,7 +1,4 @@
 $(document).ready(function() { 
-// jQuery is properly loaded at this point
-// so proceed to bind the Cordova's deviceready event
-	$(document).bind('deviceready', function() {
-		alert("Aplicacao OK!")
-	}); 
+    $(document).bind('deviceready', function() {
+    });
 });
