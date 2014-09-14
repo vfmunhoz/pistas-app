ea39bf6d185041bafa1685cdea80bef81a409c0a
Vinicius Munhoz <viniciusfmunhoz@gmail.com>
2014-09-14 15:08:50 -0300

    Pagina inicial com os dados dos proximos eventos

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,4 +1,25 @@
-$(document).ready(function() { 
-    $(document).bind('deviceready', function() {
-    });
+$(document).ready(function() {
+	$(document).bind('deviceready', function() {
+	});
+
+	$('#paginaInicialLink').click(function() {
+		var divConteudoInicial = $('#conteudoInicial');
+
+	});
 });
+
+$(document).on("click", ".show-page-loading-msg", function() {
+	var $this = $(this), theme = $this.jqmData("theme") || $.mobile.loader.prototype.options.theme, msgText = $this.jqmData("msgtext") ||
+	$.mobile.loader.prototype.options.text, textVisible = $this.jqmData("textvisible") || $.mobile.loader.prototype.options.textVisible,
+	textonly = !!$this.jqmData("textonly");
+
+	html = $this.jqmData("html") || "";
+
+	$.mobile.loading('show', {
+		text : msgText,
+		textVisible : textVisible,
+		theme : theme,
+		textonly : textonly,
+		html : html
+	});
+}); 
