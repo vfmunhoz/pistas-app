b513033240e82832ffe95687c034bae7405e7980
Vinicius Munhoz <viniciusfmunhoz@gmail.com>
2014-10-04 00:20:03 -0300

    Criado o recurso de loading

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
 	$(document).bind('deviceready', function() {
 		navigator.splashscreen.hide();
+		
+		exibirCarregando('Carregando...');
 	});
 
 	$('#paginaInicialLink').click(function() {
 		var divConteudoInicial = $('#conteudoInicial');
 	});
 });
+
+function exibirCarregando(texto) {
+	$.mobile.loading( 'show', {
+		text: texto,
+		textVisible: true,
+		theme: 'b',
+		html: ""
+	});
+}
